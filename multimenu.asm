4bee950c29773c4c7d10db2ed2fc0285d1375716
Jefferson <44975876+ZX-80@users.noreply.github.com>
2022-09-08 19:00:12 -0400

    refactor

--- multimenu.asm
+++ multimenu.asm
@@ -1,130 +1,146 @@
-    processor F8
-
+    PROCESSOR F8
     INCLUDE "ves.h"
+    ORG $0800
 
-    org $0800
-CartridgeStart: db	$55	                ; valid cart indicator
-                NOP                      ; unused byte
+CartridgeStart: 
+	; valid cart indicator
+	db	$55, $00
 
-;Before drawing, use the screen clearing subroutine in the machine ROM ( ; means comments):
-; Set Accumulator to color value
-CartridgeEntry: LI  $D6  ; grey, green $C0, blue $93,  b/w $21
-    ; Load register 3 with value from Accumulator
-    LR  3, A
-    ; Subroutine call to built-in screen clear
-    PI  $00D0
-    ; Routine clears with color set in r3
-    ; You'll end up here after return. 
+CartridgeEntry:
+	; Initialize hardware
+	lis		$0
+	outs	1
+	outs	4
+	outs	5
+	outs	0
 
-;You then load registers with:
-plot_start:
-    LI  $40      ;  colour
-    LR  1, A
-    LI  70       ; x coordinate (4-105)
-    LR  2, A
-    LI  30       ; y coordinate (4-61)
-    LR  3, A
-    PI  plot  ; Call the above plot routine
+	; Clear screen
+	li  $D6  ; grey $D6, green $C0, blue $93,  b/w $21
+    lr  3, A
+    pi  BIOS_CLEAR_SCREEN
 
-	li	20	; x
-	lr	1,a
-	li	20			; y
-	lr	2,a
-	li	$89
-	lr	0,a			; combined color + char index -> r0
-	pi	BIOS_DRAW_CHARACTER   
-
+main:
+	li  COLOR_BLUE  ; colour = blue
+    lr  1, A
+	li	40          ; X = 20
+	lr	2, A
+	li	40          ; Y = 20
+	lr	3, A
 	pi drawchar
 
-; repeat
-    jmp plot_start
+    jmp main
 
 
-; save a counter by using LISL/LISU/ISAR++/ISAR--/ISAR/BR7
-; load 7 lines into R7 to R13
-; use br7 as row counter
-; 7x7 char set, with spacing for 8x8
+;---------------;
+; Char Function ;
+;---------------;
+; draw a single char on the screen
+; r1 = set color
+; r2 = x coordinate (4-105)
+; r3 = y coordinate (4-61)
+; r7 = ascii char
+;------------------------
+; r4 = temp
+; r5 = line counter
+; r6 = row counter
+;------------------------
+charHeight: equ 8
+charWidth: equ 8
+colour_r: equ 1
+coordX_r: equ 2
+coordY_r: equ 3
+columnCounter_r: equ 5
+rowCounter_r: equ 6
+temp_r: equ 4
+drawchar: SUBROUTINE
+	li  charHeight
+	lr  rowCounter_r, A
 
-;r1 = colour
-;r2 = x
-;r3 = y
-;r4 = temp
-;r5 = line counter
-;r6 = row counter
-drawchar:
-    ; Colour = red
-	LI  $80
-    LR  1, A
+	DCI charSet
 
-    ; Y = 20
-	li	40
-	lr	3,a
+.drawRow:
+		li charWidth
+		lr columnCounter_r, A
+		LM ; A = [DC0++]
+		LR temp_r, A
 
-	; row count = 8
-	li 8
-	lr 6, A
-
-	;load bytes in and plot pixels
-	DCI char
-.get_byte:
-       
-	
-		; X = 20
-		li	40
-		lr	2,a
-
-        ; line count = 8
-		li 8
-		lr 5, A
-
-        ; A = [DC0]
-		LM
-		LR 4, A ; temp = A
-
-
-		; check 8 bits
-.check_bit
-		CI $FF
-		BM .blank_pixel
+.checkBit
+		CI $FF ; Check MSB set
+		BM .skipDraw
 			; PLOT PIXEL
-			; set the color using r1
-			lr	A, 1
+			lr	A, colour_r
 			outs	1
-			; set the column using r2
-			lr	A, 2
+			lr	A, coordX_r
 			com
 			outs	4
-			; set the row using r3
-			lr	A, 3
+			lr	A, coordY_r
 			com
 			outs	5
-			;LR 4, A ; temp = A
-			li	$60
+			li		$60
 			outs	0
-			li	$50
+			li		$50
 			outs	0
+
 			lis	6
 .plotDelay2:
 			ai	$ff
 			bnz	.plotDelay2
-.blank_pixel:
 
-		LR A, 4 ; A = temp
-		SL 1    ; A << 1
-		LR 4, A ; temp = A
+.skipDraw:
+		LR A, temp_r
+		SL 1
+		LR temp_r, A
 
-		DS 2
-		DS 5
-		BNZ .check_bit
-	DS 3
-	DS 6
-	BNZ .get_byte
+		DS coordX_r
+		DS columnCounter_r
+		BNZ .checkBit
+	
+	li 8       ; R2 += 8
+	as coordX_r
+	lr coordX_r, A
+
+	DS coordY_r
+	DS rowCounter_r
+	BNZ .drawRow
 	
 	; Return from the subroutine
 	pop
 
+asciiTooAddress:
+	; K = ascii * 8
+		; KL = ascii
+		; K << 3
+			; KL << 1
+			; KU += carry
+	; DC0 -> Q
+	; QL = KL + QL
+	; QU = KU + QU + carry
+	; DC0 <- Q
 
-char:
+
+; DC0 = charSet = 0xUU00
+; ascii = 0 to 127
+	; 011 11111 00000000
+	; 0AA BBBBB 00000000
+	; DC0 += BBBBB000
+	
+
+    ; assume A is the ascii char
+	; DC0 = charSet[8 * ascii]
+	; DCI charSet
+	; ADC
+	; ADC
+	; ADC
+	; ADC
+	; ADC
+	; ADC
+	; ADC
+	; ADC
+
+
+	ALIGN 256
+charSet: ; 0x??00
+	; Percentage
     db %00000001
     db %01100000
     db %10010100
@@ -134,43 +150,12 @@ char:
     db %00000110
     db %10000000
 
-
-;---------------;
-; Plot Function ;
-;---------------;
-; plot out a single point on the screen
-; uses three registers as "arguments"
-; r1 = set color
-; r2 = x coordinate (4-105)
-; r3 = y coordinate (4-61)
-;------------------------
-; Valid colors
-;------------------------
-; green	= $00 (%00000000)
-; red	= $40 (%01000000)
-; blue	= $80 (%10000000)
-; bkg	= $C0 (%11000000)
-;------------------------
-plot:
-	; set the color using r1
-	lr	A, 1
-	outs	1
-	; set the column using r2
-	lr	A, 2
-	com
-	outs	4
-	; set the row using r3
-	lr	A, 3
-	com
-	outs	5
-	; transfer data to the screen memory
-	li	$60
-	outs	0
-	li	$50
-	outs	0
-	; delay until it's fully updated
-	lis	6					; Value here is 4 in Videocart 21, Bowling
-.plotDelay:	
-	ai	$ff
-	bnz	.plotDelay
-	pop							; return from the subroutine
+	; Sum
+    db %00000000
+    db %00111110
+    db %00100010
+    db %00010000
+    db %00001100
+    db %00010000
+    db %00100010
+    db %00111110
